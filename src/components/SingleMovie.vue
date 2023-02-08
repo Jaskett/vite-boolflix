47b3373318cf76c47926bdedefb23c9b3a52cb9b
Brandon Vasquez <thatsjaskett@gmail.com>
2023-02-08 15:47:28 +0100

    Milestone 2 (mezza) completata

--- src/components/SingleMovie.vue
+++ src/components/SingleMovie.vue
@@ -3,6 +3,42 @@ export default {
     name: 'SingleMovie',
     props: {
         details: Object
+    },
+    data() {
+        return {
+            listFlags: [
+                {
+                    img: 'img/italy.svg',
+                    lang: "it"
+                },
+                {
+                    img: 'img/france.svg',
+                    lang: "fr"
+                },
+                {
+                    img: 'img/united_kingdom.svg',
+                    lang: "en"
+                },
+                {
+                    img: 'img/spain.svg',
+                    lang: "es"
+                },
+                {
+                    img: 'img/germany.svg',
+                    lang: "de"
+                }
+            ]
+        }
+    },
+    computed: {
+        assignFlags() {
+            for(let i = 0; i < this.listFlags.length; i++) {
+                if(this.listFlags[i].lang.includes(this.details.original_language)) {
+                    return this.listFlags[i].img;
+                }
+            }
+            return this.listFlags[this.listFlags.length - 1].img;
+        }
     }
 }
 </script>
@@ -11,8 +47,17 @@ export default {
     <!-- Template singola card -->
     <div class="movie">
         <img :src="`https://image.tmdb.org/t/p/w200/${details.poster_path}`" alt="">
-        <div>{{ details.title }}</div>
-        <div>{{ details.original_title }}</div>
+        <div v-if="details.title">
+            <div>Titolo Film: {{ details.title }}</div>
+            <div>Titolo originale film: {{ details.original_title }}</div>
+        </div>
+        <div v-else-if="details.name">
+            <div>Titolo serie tv: {{ details.name }}</div>
+            <div>Titolo originale serie tv : {{ details.original_name }}</div>
+        </div>
+        <div>
+            <img :src="assignFlags" alt="" class="bandiera">
+        </div>
         <div>Language: {{ details.original_language }}</div>
         <div>{{ details.vote_average }}</div>
     </div>
@@ -21,5 +66,9 @@ export default {
 <style lang="scss" scoped>
 .movie {
     width: calc(100% / 5);
+}
+
+.bandiera {
+    width: 40px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from "vue";
 
 export const store = reactive({
     movieList: [],
-    apiUrl: "https://api.themoviedb.org/3/movie/popular/?api_key=f0607d645ee7e294b32467feff3801ca",
-    apiSearchMovie: "https://api.themoviedb.org/3/search/movie/?api_key=f0607d645ee7e294b32467feff3801ca",
+    apiUrl: "https://api.themoviedb.org/3/movie/popular/?api_key=f0607d645ee7e294b32467feff3801ca&language=it-IT",
+    apiSearchMovie: "https://api.themoviedb.org/3/search/multi/?api_key=f0607d645ee7e294b32467feff3801ca",
     searchTitle: ""
 })
